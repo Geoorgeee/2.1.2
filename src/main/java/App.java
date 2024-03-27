ba55d4737bd4c8d2dfe6d20cd4cd403c0fdde238
goga <gosh.122012@yandex.ru>
2024-03-27 20:29:47 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,17 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1==catBean);
+        System.out.println(bean==bean1);
+    }
+}
